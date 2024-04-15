245901d0cc7e15e47de92edd51683c934e33fd8d
OdilbekBatirov <OdilbekBatirov@example.com>
2024-04-15 17:06:14 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import './App.css';
+import ToDoForm from './components/todo-form/ToDoForm';
+import TodoItem from './components/todo-item/TodoItem';
+
+const oldTodos = [
+  { id: 1, text: "Learn React" },
+  { id: 2, text: "Build a Todo App" },
+  { id: 3, text: "Master React Hooks" },
+]
+
+
+function App() {
+  const [todos, setTodos] = useState(oldTodos)
+  const handleAddTodo = (text) => {
+    const newTodo = {
+      id: todos.length + 1,
+      text: text,
+    }
+    setTodos([...todos, newTodo])
+  }
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+  return (
+   <div className="App"> 
+    <h1>Todo App</h1>
+    <ToDoForm onAddTodo={handleAddTodo} />
+
+    {
+      todos.map((todo) => {
+        <TodoItem 
+        key={todo.id}
+        id={todo.id}
+        text={todo.text}
+        onDelete={handleDelete}
+        />
+      })
+    }
+   </div>
+  )
+}
+
+export default App;
---
+++ src/components/todo-form/ToDoForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import styles from './TodoForm.module.css'
+
+const ToDoForm = ({ onAddTodo }) => {
+   const [newTodo, setNewTodo] = useState('')
+
+   const handleChange = (e) => {
+      setNewTodo(e.target.value)
+   }
+
+   const handleSubmit = (e) => {
+      e.preventDefault()
+      if (newTodo.trim() !== '') { 
+         onAddTodo(newTodo)
+         setNewTodo('')
+         
+      }
+   }
+
+   return (
+      <div>
+         <form onSubmit={handleSubmit}>
+            <input   
+               type="text"
+               className={styles.inputField}
+               value={newTodo}
+               onChange={handleChange}
+               placeholder="new todo..."
+            />
+            <button type="submit" className={styles.addButton}>
+               Add Todo
+            </button>
+         </form>
+      </div>
+   )
+}
+
+export default ToDoForm
